af28cb9e8767b480b679c589c3e8c20ff3102d01
Vatsal Patel <vatsalpatel@Vatsals-MacBook-Air.local>
2026-06-16 15:47:08 +0530

    Added variables program

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+name="ravi"
+age=69
+PI=3.44
+
+print(type(name))
+print(type(age))
+print(type(PI))
+
+print("data type of name is:",type(name))
+
+isPrime=True
+print(type(isPrime))
+isYES=False
+print(type(isYES))
+print(isPrime,isYES)
+
+X=None
+print("a value of x is",X)
+print(X)
