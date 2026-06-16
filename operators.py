528ce62bf34c3c1c0e9da1b78aeaa10482ab77e3
Vatsal Patel <vatsalpatel@Vatsals-MacBook-Air.local>
2026-06-16 17:27:27 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,41 @@
+# arithmatic
+a=10
+b=2
+
+# print("sum of a and b is:\n",a+b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b) 
+
+# relational operators
+a=10
+b=5
+print(a==b)
+print(a!=b)
+print(a>b)  
+
+# assignment operators
+a=10
+b=5
+print(a)
+a+=b
+print(a)
+a-=b
+print(a)   
+
+# logical operators
+a=True
+b=False
+print(a and b)
+print(a or b)   
+print("value of a is:", not a)
+print(not 5>8)
+
+x=10
+y=5
+print(x>y and x%2==1)
+print((x>y) or (x%2==1))
---
+++ styleguide.py
@@ -0,0 +1,5 @@
+Total_Price=100 #Pascal Case
+total_Price=100 #camel Case
+total_price=100 #snake case
+
+#snake case is most widely used in python
