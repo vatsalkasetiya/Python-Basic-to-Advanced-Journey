9f9ecf85fb42c409a66fecc66208dd89c9d24d61
Vatsal Patel <vatsalpatel@Vatsals-MacBook-Air.local>
2026-06-17 09:35:47 +0530

    nesting

---
+++ nesting.py
@@ -0,0 +1,19 @@
+# age=int(input("enter your age:"))
+# python=False
+
+# if age>=18:
+#     if python==True:
+#         print("select")
+#     else:
+#         print("python is weak")
+# else:
+#     print("not graduate")
+
+marks=int(input("enter your marks:"))
+if (marks>=35):
+    if(marks>=90):
+        print("grade A")
+    else:
+        print("pass")
+else:
+    print("fail")
