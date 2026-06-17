907f1bb196b2a3601fbe3e440dfa703c3384d8b4
Vatsal Patel <vatsalpatel@Vatsals-MacBook-Air.local>
2026-06-17 17:28:36 +0530

    while loop

---
+++ while.py
@@ -0,0 +1,120 @@
+# i=1
+# while(i<=10):
+#     print(i)
+#     i+=1
+
+# basic couting
+
+# i=1
+# while(i<=5):
+#     print(i)
+#     i+=1
+
+# even numbers
+
+# i=2
+# while(i<=10):
+#     print(i)
+#     i+=2
+
+# reverse counting
+
+# i=10
+# while(i>=1):
+#     print(i)
+#     i-=1            
+
+
+# user controlled loop
+
+# password=" "
+
+# while (password!="python123"):
+#     password=input("enter the password:")
+# print("login successful")
+     
+
+#break
+
+# i=1
+# while(i<=10):
+#     if(i==5):
+#         break
+#     print(i)
+#     i+=1
+
+
+# n=" "
+# while(n!=100598):
+#     n=int(input("enter right password:"))
+#     print(n)
+
+
+# while True:
+#     n=int(input("enter a number:"))
+#     if(n==100598):
+#         print("login successful")
+#         break
+#     else:
+#         print("try again")
+        
+
+#continue statement
+
+# i=1
+# while i<=10:
+#     if i==3:
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1    
+    
+# i=0
+# while(i<=20):
+#     if(i%2==0):
+#         print(i)
+#     else:
+#         i+=1
+#         continue
+
+#     i+=1    
+
+# i=1
+# while(i<=15):
+#     if(i%3==0):
+#         print(i)
+#     else:
+#         i+=1
+#         continue
+    
+#     i+=1
+    
+
+
+# i=1
+# while(i<=5):
+#     num=int(input("enter number:"))
+#     if(num<0):
+#         i+=1
+#         continue
+#     else:
+#         print("print:",num)
+#     i+=1
+
+# i=0
+# word="pythondeveloper"
+# while(i<=14):
+#     if(word[i]=="a" or word[i]=="e" or word[i]=="i" or word[i]=="o" or word[i]=="u"):
+#         i+=1
+#         continue
+#     print(word[i])
+#     i+=1
+i=1
+num=int(input("enter number n:"))
+while(i<=10):
+    if(i==5):
+        break
+    print(num,"*",i,"=",i*num)
+    i+=1
+        
+
