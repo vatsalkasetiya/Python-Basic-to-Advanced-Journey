9a3ee1aa600c13b289a72da29f13f67a7ae09908
Vatsal Patel <vatsalpatel@Vatsals-MacBook-Air.local>
2026-06-17 09:21:29 +0530

    conditional practise

---
+++ conditional_prac.py
@@ -0,0 +1,31 @@
+# age=int(input("enter your age:"))
+# if (age>=18):
+#     print("adult")
+# elif(age>=13 and age<18):
+#     print("teenager")
+# else:
+#     print("child")
+
+# username=input("enter your username:")
+# password=input("enter your password:")
+
+# if username=="vatsal" and password=="1234":
+#     print("login successful")
+# elif username!="vatsal" and password=="1234":
+#     print("invalid username")
+# elif username=="vatsal" and password!="1234":
+#     print("invalid password")
+# else:
+#     print("invalid username and password")
+
+# n=int(input("enter a number:"))
+# if (n%5==0):
+#     print("number is divisible by 5")
+# else:   
+#     print("number is not divisible by 5")
+
+n=int(input("enter a number:"))
+if(n%2==0):
+    print("number is even")
+else:
+    print("number is odd")
